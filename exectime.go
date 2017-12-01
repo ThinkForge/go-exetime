7845b8dc494ce826983ca198d0cede356981d597
SheaRogers <shearogers@thinkforge.io>
2017-11-30 19:52:47 -0500

    Begin work on the multiple input tests.

--- exectime.go
+++ exectime.go
@@ -2,28 +2,37 @@ package exectime
 
 import (
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func exectime(folder string) (timeresult, error) {
-	arr := strings.Split(folder, "/")
-	path, err := exec.LookPath(folder + arr[len(arr)-2] + ".cut.out")
 
-	if err != nil {
-		return timeresult{ratio: nil}, err
+	// Store the time for each run.
+	var cutTimeArr, exTimeArr [5]int64
+
+	// Parse the filepath into slices and then get the files.
+	arr := strings.Split(folder, "/")
+	pathCut, errCut := exec.LookPath(folder + arr[len(arr)-2] + ".cut.out")
+	pathEx, errEx := exec.LookPath(folder + arr[len(arr)-2] + ".ex.out")
+
+	// If either of the files could not be found, return the error.
+	if errCut != nil {
+		return timeresult{ratio: nil}, errCut
+	} else if errEx != nil {
+		return timeresult{ration: nil}, errEx
 	}
 
-	cmd := exec.Command(path)
+	// For each of the input files we evaluate it using both executables.
+	for i := 0; i < 5; i++ {
+		cmd := exec.Command(pathCut, folder+strconv.Itoa(i)+".txt")
+		started := start()
+		cmd.Run()
+		time := end(started)
 
-	started := start()
-
-	cmd.Run()
-
-	time := end(started)
-
-	result := timeresult{cutTime: time, exTime: time}
-
+		result := timeresult{cutTime: time, exTime: time}
+	}
 	return result, nil
 }
 
@@ -37,5 +46,5 @@ func end(startTime time.Time) int64 {
 }
 
 type timeresult struct {
-	ratio int64
+	ratio [5]int64
 }
